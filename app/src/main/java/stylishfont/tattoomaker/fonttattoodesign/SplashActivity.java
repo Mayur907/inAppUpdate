45c2fa398cd86bed0166f22792e074fdfcbfada3
admin <realmemodel6@gmail.com>
2023-02-21 17:31:50 +0530

    bug fix 21/2/2023-5:30PM

--- app/src/main/java/stylishfont/tattoomaker/fonttattoodesign/SplashActivity.java
+++ app/src/main/java/stylishfont/tattoomaker/fonttattoodesign/SplashActivity.java
@@ -8,6 +8,13 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnFailureListener;
+import com.google.android.play.core.tasks.Task;
+
 import inappupdate.updateimmediate.updateflexible.InAppUpdate;
 import inappupdate.updateimmediate.updateflexible.Type;
 
@@ -19,9 +26,30 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /* check if update is available or not */
+        AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(this);
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+        appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE) {
+                startInAppUpdateActivity();
+            } else {
+                createTimer(COUNTER_TIME);
+            }
+            Log.e("update", " addOnSuccessListener ");
+        });
+
+        appUpdateInfoTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Log.e("update", " addOnFailureListener ");
+                createTimer(COUNTER_TIME);
+            }
+        });
+    }
+
+    private void startInAppUpdateActivity() {
         Intent intent = new Intent(this, InAppUpdate.class);
-        intent.putExtra("key", Type.IMMEDIATE);
-//        intent.putExtra("key", Type.FLEXIBLE);
+        intent.putExtra("key", Type.IMMEDIATE); /* Type.IMMEDIATE, Type.FLEXIBLE */
         startActivityForResult(intent, 123);
     }
 
@@ -47,15 +75,12 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.e("update", "splash requestCode :: " + requestCode);
-        Log.e("update", "splash resultCode :: " + resultCode);
         if (requestCode == 123) {
             if (resultCode == RESULT_OK) {
                 createTimer(COUNTER_TIME);
             } else /*resultCode == RESULT_CANCELED*/ {
                 if (data != null) {
-                    Log.e("update", "splash from :: " + data.getIntExtra("from", 0));
-                    /*check return from immediate or flexible
+                    /* check return from immediate or flexible
                      * if return from immediate so app close */
                     if (data.getIntExtra("from", 0) == 123456) {
                         finish();
--- inappupdate/src/main/java/inappupdate/updateimmediate/updateflexible/InAppUpdate.java
+++ inappupdate/src/main/java/inappupdate/updateimmediate/updateflexible/InAppUpdate.java
@@ -2,18 +2,16 @@ package inappupdate.updateimmediate.updateflexible;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.os.Bundle;
-import android.util.Log;
+//import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.play.core.appupdate.AppUpdateInfo;
 import com.google.android.play.core.appupdate.AppUpdateManager;
 import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
@@ -35,17 +33,16 @@ public class InAppUpdate extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.e("update", "onCreate ");
-
+//        Log.e("update", "onCreate ");
         Bundle extras = getIntent().getExtras();
         if ((extras != null) && (extras.containsKey("key")))
-            type = (Type)extras.getSerializable("key");
+            type = (Type) extras.getSerializable("key");
 
         appUpdateManager = AppUpdateManagerFactory.create(this);
         checkType(type);
     }
 
-    private void checkType(Type type){
+    private void checkType(Type type) {
         if (type == Type.IMMEDIATE) {
             immediateUpdate();
         } else {
@@ -54,7 +51,7 @@ public class InAppUpdate extends AppCompatActivity {
     }
 
     /*flexible update*/
-    public void flexibleUpdate(){
+    public void flexibleUpdate() {
         installStateUpdatedListener = state -> {
             if (state.installStatus() == InstallStatus.DOWNLOADED) {
                 popupSnackBarForCompleteUpdate();
@@ -73,30 +70,31 @@ public class InAppUpdate extends AppCompatActivity {
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
                     && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
                 startUpdateFlowFlexible(appUpdateInfo);
-                Log.e("update", " UPDATE_AVAILABLE ");
+//                Log.e("update", " UPDATE_AVAILABLE ");
             } else if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
                 popupSnackBarForCompleteUpdate();
-                Log.e("update", " DOWNLOADED ");
+//                Log.e("update", " DOWNLOADED ");
             } else {
-                Log.e("update", " else fail ");
+//                Log.e("update", " else fail ");
                 Intent intent = new Intent();
-                setResult(Activity.RESULT_CANCELED,intent);
+                setResult(Activity.RESULT_CANCELED, intent);
                 finish();
             }
-            Log.e("update", " addOnSuccessListener ");
+//            Log.e("update", " addOnSuccessListener ");
         });
 
         appUpdateInfoTask.addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(Exception e) {
-                Log.e("update", " addOnFailureListener ");
+//                Log.e("update", " addOnFailureListener ");
                 Intent intent = new Intent();
-                setResult(Activity.RESULT_CANCELED,intent);
+                setResult(Activity.RESULT_CANCELED, intent);
                 finish();
-                Log.e("update", " addOnFailureListener ");
+//                Log.e("update", " addOnFailureListener ");
             }
         });
     }
+
     private void startUpdateFlowFlexible(AppUpdateInfo appUpdateInfo) {
         try {
             appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.FLEXIBLE, this, RC_APP_UPDATE_FLEXIBLE);
@@ -104,6 +102,7 @@ public class InAppUpdate extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
     private void popupSnackBarForCompleteUpdate() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Alert!");
@@ -137,11 +136,13 @@ public class InAppUpdate extends AppCompatActivity {
 //                .setActionTextColor(getResources().getColor(android.R.color.white))
 //                .show();
     }
+
     private void removeInstallStateUpdateListener() {
         if (appUpdateManager != null) {
             appUpdateManager.unregisterListener(installStateUpdatedListener);
         }
     }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -165,67 +166,68 @@ public class InAppUpdate extends AppCompatActivity {
     }
 
     /*immediate update*/
-    public void immediateUpdate(){
+    public void immediateUpdate() {
         Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
         appUpdateInfoTask.addOnSuccessListener(appUpdateInfo -> {
             if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
                     && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
-                Log.e("update", " UPDATE_AVAILABLE ");
+//                Log.e("update", " UPDATE_AVAILABLE ");
                 startUpdateFlowImmediate(appUpdateInfo);
             } else if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
-                Log.e("update", " DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS ");
+//                Log.e("update", " DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS ");
                 startUpdateFlowImmediate(appUpdateInfo);
             } else {
-                Log.e("update", " else fail ");
+//                Log.e("update", " else fail ");
                 Intent intent = new Intent();
-                setResult(Activity.RESULT_CANCELED,intent);
+                setResult(Activity.RESULT_CANCELED, intent);
                 finish();
             }
-            Log.e("update", " addOnSuccessListener ");
+//            Log.e("update", " addOnSuccessListener ");
         });
 
         appUpdateInfoTask.addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(Exception e) {
-                Log.e("update", " addOnFailureListener ");
+//                Log.e("update", " addOnFailureListener ");
                 Intent intent = new Intent();
-                setResult(Activity.RESULT_CANCELED,intent);
+                setResult(Activity.RESULT_CANCELED, intent);
                 finish();
             }
         });
     }
+
     private void startUpdateFlowImmediate(AppUpdateInfo appUpdateInfo) {
         try {
             appUpdateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, this, RC_APP_UPDATE_IMMEDIATE);
         } catch (IntentSender.SendIntentException e) {
             e.printStackTrace();
-            Log.e("update"," error :: " + e.getMessage());
+//            Log.e("update", " error :: " + e.getMessage());
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.e("update", "inapp requestCode :: " + requestCode);
-        Log.e("update", "inapp resultCode :: " + resultCode);
+//        Log.e("update", "inapp requestCode :: " + requestCode);
+//        Log.e("update", "inapp resultCode :: " + resultCode);
         if (requestCode == RC_APP_UPDATE_IMMEDIATE || requestCode == RC_APP_UPDATE_FLEXIBLE) {
             if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(getApplicationContext(), "Update canceled by user! Result Code: " + resultCode, Toast.LENGTH_LONG).show();
-                Log.e("update", " cancel ");
+//                Log.e("update", " cancel ");
                 Intent intent = new Intent();
                 intent.putExtra("from", requestCode);
-                setResult(Activity.RESULT_CANCELED,intent);
+                setResult(Activity.RESULT_CANCELED, intent);
                 finish();
 
             } else if (resultCode == RESULT_OK) {
                 Toast.makeText(getApplicationContext(), "Update success! Result Code: " + resultCode, Toast.LENGTH_LONG).show();
-                Log.e("update", " ok ");
+//                Log.e("update", " ok ");
                 Intent intent = new Intent();
-                setResult(Activity.RESULT_OK,intent);
+                setResult(Activity.RESULT_OK, intent);
                 finish();
             } else {
                 Toast.makeText(getApplicationContext(), "Update Failed! Result Code: " + resultCode, Toast.LENGTH_LONG).show();
-                Log.e("update", " else ");
+//                Log.e("update", " else ");
                 checkType(type);
             }
         }
